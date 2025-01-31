2d7521c2582a4bb203e40e478fad2ebdf4e03b1c
Maxime Faure <maxime.faure93@gmail.com>
2025-01-31 19:40:16 +0100

    [UPD] - Footer style

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -2,7 +2,7 @@ import { Section } from "./Section";
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-24 border-t">
+    <footer className="flex items-center justify-center w-full h-24 ">
       <Section>
         <p className="text-sm text-muted-foreground">
           &copy; 2025 - Built with Next.js, TypeScript, and Tailwind CSS
