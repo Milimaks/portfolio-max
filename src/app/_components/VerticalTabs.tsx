d0895a2d6a0e18ddec0428e410a84914ccf5db99
Maxime Faure <maxime.faure93@gmail.com>
2025-02-05 14:25:01 +0100

    [UPD] - VerticalTabs mobile version

--- src/app/_components/VerticalTabs.tsx
+++ src/app/_components/VerticalTabs.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { ChevronRight } from "lucide-react";
+import { ChevronDown, ChevronRight } from "lucide-react";
 
 interface TabItem {
   id: string;
@@ -16,20 +16,25 @@ export const VerticalTabs: React.FC<VerticalTabsProps> = ({ items }) => {
   const [activeTab, setActiveTab] = useState(items[0].id);
 
   return (
-    <div className="flex min-h-[400px] text-gray-300">
-      <div className="w-48 border-r border-gray-700">
+    <div className="flex  min-h-[400px] text-gray-300 max-md:flex-col">
+      <div className="w-full md:w-48 border-b md:border-b-0 md:border-r border-gray-700 overflow-x-auto max-md:flex max-md:border-r-0 max-md:border-b">
         {items.map((item) => (
           <button
             key={item.id}
             onClick={() => setActiveTab(item.id)}
             className={`flex items-center w-full px-4 py-3 text-left transition-colors ${
               activeTab === item.id
-                ? "bg-gray-200 border-l-2 border-primary font-heading text-primary"
-                : "hover:bg-gray-200 border-l-2 border-transparent"
+                ? "bg-gray-200 border-b-2 md:border-b-0 md:border-l-2 border-primary font-heading text-primary"
+                : "hover:bg-gray-200 border-b-2 md:border-b-0 md:border-l-2  border-transparent"
             }`}
           >
+            <ChevronDown
+              className={`w-4 h-4 mr-2 transition-opacity hidden max-md:block  ${
+                activeTab === item.id ? "opacity-100 text-primary" : "opacity-0"
+              }`}
+            />
             <ChevronRight
-              className={`w-4 h-4 mr-2 transition-opacity ${
+              className={`w-4 h-4 mr-2 transition-opacity max-md:hidden ${
                 activeTab === item.id ? "opacity-100 text-primary" : "opacity-0"
               }`}
             />
