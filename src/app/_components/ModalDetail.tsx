ca2d9d5c2f0bcbd2e2acacba560b13cbcca0869f
Maxime Faure <maxime.faure93@gmail.com>
2025-02-10 14:27:00 +0100

    [ADD + UPD] - scroll bar design and modal style update

--- src/app/_components/ModalDetail.tsx
+++ src/app/_components/ModalDetail.tsx
@@ -19,7 +19,7 @@ export const ModalDetail: React.FC<ModalDetailProps> = ({
   <Modal isOpen={isOpen} onClose={onClose}>
     <h2 className="text-xl sm:text-2xl font-bold text-white mb-4">{title}</h2>
     <Carousel images={images} />
-    <div className="mt-4 sm:mt-6 text-gray-300">
+    <div className="mt-4 sm:mt-6 text-black">
       <p className="text-sm sm:text-base whitespace-pre-line">{description}</p>
     </div>
   </Modal>
