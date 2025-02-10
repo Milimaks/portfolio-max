aee02a12991ec7fc417e19489d39fc6270cb6de6
Maxime Faure <maxime.faure93@gmail.com>
2025-02-10 14:09:55 +0100

    [ADD] - Data modal

---
+++ src/data/dataModal.ts
@@ -0,0 +1,110 @@
+export const missions = {
+  HubeeConnect: {
+    title: "Développeur Front-end @ HubeeConnect",
+    description: `En tant que développeur front-end, j'étais membre d'une équipe de trois développeurs front-end travaillant sur la version desktop de la solution.
+
+Responsabilités principales :
+
+• Développement de la partie Front-end du Saas HubeeConnect
+- Création d'interfaces utilisateur réactives et performantes en React / MUI
+- Rédaction de la documentation technique
+- Optimisation des performances (Memo, useMemo, useCallback, etc.)
+
+• Gestion des requêtes API
+- Utilisation de graphQL pour les requêtes et mutations
+- Utilisation de Apollo Client pour la gestion des données
+- State management avec useContext 
+
+• Gestion de projet
+- Utilisation de ClickUp pour la gestion des tâches
+- Méthode de travail Agile Scrum avec des sprints de 2 semaines
+- Utilisation de gitLab pour les merge requests et le versionning`,
+    images: [
+      {
+        url: "/Hubeecar_1.jpg",
+        alt: "Dashboard de l'application",
+      },
+
+      {
+        url: "/Hubeecar_3.jpg",
+        alt: "Dashboard de l'application",
+      },
+      {
+        url: "/Hubeecar_4.jpg",
+        alt: "Dashboard de l'application",
+      },
+    ],
+  },
+  sncf_uas: {
+    title: "Développeur fullstack Drupal / Php @ SNCF - Usine A Sites",
+    description: `Développeur Drupal / Php au sein d'une équipe, l'objectif du projet était de créer les composants d'un site de base réutilisable et customisable pour permettre la duplication de sites sur de nouveaux projets sans avoir besoin d'utiliser du code.
+
+Responsabilités principales :
+
+• Développement de la partie Front-end et Back-end
+- Création des composants en Drupal 8
+- Modification du front par override des templates Twig ou via préprocession des templates
+- Focus sur l'expérience utilisateur et l'accessibilité
+- Utilisation de jQuery pour les interactions
+
+• Collaboration et suivi
+- Cahier des charges et spécifications techniques à respecter
+- Système de tickets et de suivi des tâches sur Jira
+- Versionning et gestion de code avec GitLab
+- Méthode de travail Agile Scrum avec des sprints de 2 semaines,
+`,
+    images: [
+      {
+        url: "/UAS_1.png",
+        alt: "usine à sites SNCF",
+      },
+      {
+        url: "/UAS_2.png",
+        alt: "usine à sites SNCF",
+      },
+      {
+        url: "/UAS_3.png",
+        alt: "usine à sites SNCF",
+      },
+      {
+        url: "/UAS_4.png",
+        alt: "usine à sites SNCF",
+      },
+    ],
+  },
+  sncf_publimmo: {
+    title: "Développeur fullstack Drupal / Php @ SNCF - E-Publimmo",
+    description: `Refonte graphique complète d'un site de locations immobilières pour professionnels en drupal pour la SNCF.
+
+Responsabilités principales :
+
+• Développement frontend
+- Composants "recherche", "map", "carousel" et "fiche info" en Drupal 9, CSS et JS
+- Gestion des alertes et des notifications
+- Maintient de l'ancienne architecture projet et des données actives
+
+• Collaboration et suivi
+- Cahier des charges et spécifications techniques à respecter
+- Système de tickets et de suivi des tâches sur Jira
+- Versionning et gestion de code avec GitLab
+- Méthode de travail Agile Scrum avec des sprints de 2 semaines`,
+    images: [
+      {
+        url: "/publimmo_1.png",
+        alt: "e-publimmo SNCF",
+      },
+      {
+        url: "/publimmo_2.png",
+        alt: "e-publimmo SNCF",
+      },
+      {
+        url: "/publimmo_3.png",
+        alt: "e-publimmo SNCF",
+      },
+      {
+        url: "/publimmo_4.png",
+        alt: "e-publimmo SNCF",
+      },
+    ],
+  },
+};
